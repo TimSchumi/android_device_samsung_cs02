96ebb1e8eb8fcf662d727853aa50219b98c2cb75
Tim Schumacher <timschumi2@arcor.de>
2017-10-02 23:14:59 +0200

    Enable TW_INCLUDE_CRYPTO to build libcrypto
    Change-Id: I303c11c0f75701f6bf77536868cae1901e9e92a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ BOARD_SUPPRESS_EMMC_WIPE := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TW_INCLUDE_CRYPTO := true
 
 # jemalloc causes a lot of random crash on free()
 MALLOC_IMPL := dlmalloc
